fbfa4c30e2424bf414ecfe7773296f55211eb022
shubh <shahshubh1010@gmail.com>
2020-09-25 12:40:22 +0530

    display full image
    Former-commit-id: 47b919b70167fc33ba50c718b2e44a4134502664

--- lib/widgets/FullImageWidget.dart
+++ lib/widgets/FullImageWidget.dart
@@ -5,20 +5,43 @@ class FullPhoto extends StatelessWidget {
   final String url;
   FullPhoto({Key key, @required this.url}) : super(key: key);
   @override
-  Widget build(BuildContext context) {}
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.lightBlue,
+        iconTheme: IconThemeData(color: Colors.white),
+        title: Text(
+          "Full Image",
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+      ),
+      body: FullPhotoScreen(url: url),
+    );
+  }
 }
 
 class FullPhotoScreen extends StatefulWidget {
+  final String url;
+  FullPhotoScreen({Key key, @required this.url}) : super(key: key);
+
   @override
-  State createState() => FullPhotoScreenState();
+  State createState() => FullPhotoScreenState(url: url);
 }
 
 class FullPhotoScreenState extends State<FullPhotoScreen> {
+  final String url;
+  FullPhotoScreenState({Key key, @required this.url});
+
   @override
   void initState() {
     super.initState();
   }
 
   @override
-  Widget build(BuildContext context) {}
+  Widget build(BuildContext context) {
+    return Container(
+      child: PhotoView(imageProvider: NetworkImage(url)),
+    );
+  }
 }
