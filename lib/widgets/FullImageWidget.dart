53a46ae12339e9ba0e62c51fa7f16570622c902e
shubh <shahshubh1010@gmail.com>
2020-09-25 12:31:46 +0530

    display messages
    Former-commit-id: ba9a2ebcef2c98dc1de6950f50db4ca7d4bb001e

--- lib/widgets/FullImageWidget.dart
+++ lib/widgets/FullImageWidget.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:photo_view/photo_view.dart';
 
 class FullPhoto extends StatelessWidget {
+  final String url;
+  FullPhoto({Key key, @required this.url}) : super(key: key);
   @override
   Widget build(BuildContext context) {}
 }
