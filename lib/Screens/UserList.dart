49cd5a3d0a98bd3f6d3237358e5889781bce74e0
shubh <shahshubh1010@gmail.com>
2020-09-26 18:49:45 +0530

    added search
    Former-commit-id: c4415faf7dbbce569ef8026db0f38e4fcde0f023

--- lib/Screens/UserList.dart
+++ lib/Screens/UserList.dart
@@ -1,9 +1,9 @@
 import 'package:ChatApp/Models/user.dart';
+import 'package:ChatApp/Screens/ChattingPage.dart';
 import 'package:ChatApp/constants.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:ChatApp/components/chat.dart';
-import 'package:ChatApp/models/chat_users.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class UserList extends StatefulWidget {
@@ -30,64 +30,166 @@ class _UserListState extends State<UserList> {
     });
   }
 
-  // _getUsers() async {
-  //   await getCurrUserId();
-  //   QuerySnapshot querySnapshot =
-  //       await Firestore.instance.collection("Users").getDocuments();
-
-  //   querySnapshot.documents.forEach((element) {
-  //     if (element.data["uid"] != currentuserid) {
-  //       allUsers.add(element.data);
-  //     }
-  //   });
-
-  //   setState(() {});
-  // }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Chat App'),
+        backgroundColor: kPrimaryColor,
+        centerTitle: true,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+              showSearch(
+                  context: context,
+                  delegate: DataSearch(allUsersList: allUsersList));
+            },
+          )
+        ],
+      ),
       body: SingleChildScrollView(
         physics: BouncingScrollPhysics(),
-        child: StreamBuilder(
-          stream: Firestore.instance.collection("Users").snapshots(),
-          builder: (context, snapshot) {
-            if (!snapshot.hasData) {
-              return Container(
-                child: Center(
-                  child: CircularProgressIndicator(
-                      valueColor: AlwaysStoppedAnimation(
-                    kPrimaryColor,
-                  )),
-                ),
-                height: MediaQuery.of(context).copyWith().size.height -
-                    MediaQuery.of(context).copyWith().size.height / 5,
-                width: MediaQuery.of(context).copyWith().size.width,
-              );
-            } else {
-              snapshot.data.documents
-                  .removeWhere((i) => i["uid"] == currentuserid);
-              allUsersList = snapshot.data.documents;
-              return ListView.builder(
-                padding: EdgeInsets.only(top: 16),
-                itemCount: snapshot.data.documents.length,
-                shrinkWrap: true,
-                physics: NeverScrollableScrollPhysics(),
-                itemBuilder: (context, index) {
-                  return ChatUsersList(
-                      name: snapshot.data.documents[index]["name"],
-                      secondaryText: "Secondary Text",
-                      image: snapshot.data.documents[index]["photoUrl"],
-                      time: snapshot.data.documents[index]["createdAt"],
-                      isMessageRead: true,
-                      userId: snapshot.data.documents[index]["uid"],
-                      screen: "UserListScreen");
-                },
-              );
-            }
-          },
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            StreamBuilder(
+              stream: Firestore.instance.collection("Users").snapshots(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return Container(
+                    child: Center(
+                      child: CircularProgressIndicator(
+                          valueColor: AlwaysStoppedAnimation(
+                        kPrimaryColor,
+                      )),
+                    ),
+                    height: MediaQuery.of(context).copyWith().size.height -
+                        MediaQuery.of(context).copyWith().size.height / 5,
+                    width: MediaQuery.of(context).copyWith().size.width,
+                  );
+                } else {
+                  snapshot.data.documents
+                      .removeWhere((i) => i["uid"] == currentuserid);
+                  allUsersList = snapshot.data.documents;
+                  return ListView.builder(
+                    padding: EdgeInsets.only(top: 16),
+                    itemCount: snapshot.data.documents.length,
+                    shrinkWrap: true,
+                    physics: NeverScrollableScrollPhysics(),
+                    itemBuilder: (context, index) {
+                      return ChatUsersList(
+                          name: snapshot.data.documents[index]["name"],
+                          secondaryText: "Secondary Text",
+                          image: snapshot.data.documents[index]["photoUrl"],
+                          time: snapshot.data.documents[index]["createdAt"],
+                          isMessageRead: true,
+                          userId: snapshot.data.documents[index]["uid"],
+                          screen: "UserListScreen");
+                    },
+                  );
+                }
+              },
+            ),
+          ],
         ),
       ),
     );
+  }
+}
+
+class DataSearch extends SearchDelegate {
+  DataSearch({this.allUsersList});
+  var allUsersList;
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [
+      IconButton(
+        icon: Icon(Icons.clear),
+        onPressed: () {
+          query = "";
+        },
+      )
+    ];
+    // Actions for AppBar
+    throw UnimplementedError();
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    // Leading Icon on left of appBar
+    return IconButton(
+      icon: AnimatedIcon(
+        icon: AnimatedIcons.menu_arrow,
+        progress: transitionAnimation,
+      ),
+      onPressed: () {
+        close(context, null);
+      },
+    );
+    throw UnimplementedError();
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    // show some result based on selection
+
+    throw UnimplementedError();
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    // Show when someone searches for something
+    var userList = [];
+    allUsersList.forEach((e) {
+      userList.add(e);
+    });
+    var suggestionList = userList;
+
+    if (query.isNotEmpty) {
+      suggestionList = [];
+      userList.forEach((element) {
+        if (element["name"].toLowerCase().startsWith(query.toLowerCase())) {
+          suggestionList.add(element);
+        }
+      });
+    }
+
+    // suggestionList = query.isEmpty
+    //     ? suggestionList
+    //     : suggestionList
+    //         .where((element) => element.startsWith(query.toLowerCase()))
+    //         .toList();
+
+    return ListView.builder(
+        itemBuilder: (context, index) => ListTile(
+              onTap: () {
+                close(context, null);
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return Chat(
+                      receiverId: suggestionList[index]["uid"],
+                      receiverAvatar: suggestionList[index]["photoUrl"],
+                      receiverName: suggestionList[index]["name"]);
+                }));
+              },
+              leading: Icon(Icons.person),
+              title: RichText(
+                text: TextSpan(
+                    text: suggestionList[index]["name"]
+                        .toLowerCase()
+                        .substring(0, query.length),
+                    style: TextStyle(
+                        color: Colors.black, fontWeight: FontWeight.bold),
+                    children: [
+                      TextSpan(
+                          text: suggestionList[index]["name"]
+                              .toLowerCase()
+                              .substring(query.length),
+                          style: TextStyle(color: Colors.grey))
+                    ]),
+              ),
+            ),
+        itemCount: suggestionList.length);
+    throw UnimplementedError();
   }
 }
